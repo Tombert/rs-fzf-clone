4f0bd714403d6aa9aef394127d45d05001708ddc
Thomas Gebert <thomas@gebert.app>
2025-04-22 19:50:53 -0400

    Break up into two files to declutter a bit.

---
+++ src/helpers.rs
@@ -0,0 +1,263 @@
+use crossterm::event::{self, EnableMouseCapture, Event, KeyCode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use ratatui::backend::CrosstermBackend;
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::style::{Modifier, Style};
+
+
+use crossterm::execute;
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::Terminal;
+use ratatui::style::Color;
+use ratatui::text::{Line, Span, Text};
+use ratatui::widgets::Paragraph;
+use ratatui::widgets::{Block, Borders, List, ListItem, ListState};
+
+use rayon::prelude::*;
+use std::io::{self, BufRead};
+use std::time::Duration;
+
+fn fuzzy_search(input: &str, line: &str) -> Option<(String, Vec<usize>)> {
+    let mut input_index = 0;
+    let input_chars: Vec<char> = input.chars().collect();
+    let input_length = input.len();
+    let mut counter = 0;
+    let mut hits: Vec<usize> = Vec::new();
+    let line_length = line.len();
+    if input_length > 0 {
+        for (i, cc) in line.chars().enumerate() {
+            let current = input_chars.get(input_index).unwrap().to_ascii_lowercase();
+
+            if cc.to_ascii_lowercase() == current {
+                input_index += 1;
+                hits.push(counter);
+
+                if input_index >= input_length {
+                    break;
+                }
+            } else if i == line_length - 1 {
+                return None;
+            }
+
+            counter += 1;
+        }
+    }
+
+    if input_index < input_length {
+        return None;
+    }
+    return Some((line.to_string(), hits));
+}
+
+fn get_delta(input: &Vec<usize>) -> usize {
+    let mut delta = 0;
+    if input.len() > 1 {
+        for i in input.windows(2) {
+            let first = i[0];
+            let second = i[1];
+            delta += second - first;
+        }
+    }
+
+    delta
+}
+
+fn do_filter(
+    filtered_lines: &mut Vec<(String, Vec<usize>)>,
+    all_lines: &Vec<(String, Vec<usize>)>,
+    input: &String,
+    selected: &mut Option<usize>,
+) {
+    let mut filtered_lines2: Vec<(String, Vec<usize>)> = all_lines
+        .par_iter()
+        .filter_map(|(line, _)| fuzzy_search(&input, line))
+        .collect();
+
+    filtered_lines2.par_sort_by_key(|(_, hits)| get_delta(hits));
+    filtered_lines2.reverse();
+
+    *filtered_lines = filtered_lines2;
+
+    if !filtered_lines.is_empty() {
+        *selected = Some(filtered_lines.len() - 1);
+    } else {
+        *selected = None;
+    }
+}
+
+
+pub fn do_handle(
+    cursor_position: &mut usize,
+    input: &mut String,
+    filtered_lines: &mut Vec<(String, Vec<usize>)>,
+    all_lines: &Vec<(String, Vec<usize>)>,
+    selected: &mut Option<usize>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    match event::read()? {
+        Event::Key(key) => {
+            enum Action {
+                MoveLeft,
+                MoveRight,
+                MoveUp,
+                MoveDown,
+                MoveEnd,
+                MoveBegin,
+                Exit,
+                Select,
+                ClearAll,
+                Other,
+            }
+
+            let action = match key.code {
+                KeyCode::Enter => Action::Select,
+                KeyCode::Esc => Action::Exit,
+                KeyCode::Char('u')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::ClearAll
+                }
+                KeyCode::Char('c')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::Exit
+                }
+                KeyCode::Char('e')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::MoveEnd
+                }
+                KeyCode::Char('a')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::MoveBegin
+                }
+
+                KeyCode::Up => Action::MoveUp,
+                KeyCode::Char('p')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::MoveUp
+                }
+                KeyCode::Down => Action::MoveDown,
+                KeyCode::Char('n')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::MoveDown
+                }
+                KeyCode::Left => Action::MoveLeft,
+                KeyCode::Char('b')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::MoveLeft
+                }
+                KeyCode::Right => Action::MoveRight,
+                KeyCode::Char('f')
+                    if key
+                        .modifiers
+                        .contains(crossterm::event::KeyModifiers::CONTROL) =>
+                {
+                    Action::MoveRight
+                }
+                _ => Action::Other,
+            };
+
+            match action {
+                Action::ClearAll => {
+                    *cursor_position = 0;
+                    input.clear();
+                    do_filter(filtered_lines, &all_lines, &input, selected);
+                    Ok(())
+                }
+                Action::Select => {
+                    if let Some(sel) = selected {
+                        if let Some(line) = filtered_lines.get(*sel) {
+                            disable_raw_mode()?;
+                            execute!(io::stderr(), LeaveAlternateScreen)?;
+                            println!("{}", line.0);
+                            std::process::exit(0);
+                        }
+                    }
+                    Ok(())
+                }
+                Action::Exit => {
+                    disable_raw_mode()?;
+                    execute!(io::stderr(), LeaveAlternateScreen)?;
+                    std::process::exit(0);
+                }
+                Action::MoveBegin => {
+                    *cursor_position = 0;
+                    Ok(())
+                }
+                Action::MoveEnd => {
+                    *cursor_position = input.len();
+                    Ok(())
+                }
+                Action::MoveLeft => {
+                    if *cursor_position > 0 {
+                        *cursor_position -= 1;
+                    }
+                    Ok(())
+                }
+                Action::MoveRight => {
+                    if *cursor_position < input.len() {
+                        *cursor_position += 1;
+                    }
+                    Ok(())
+                }
+                Action::MoveUp => {
+                    if let Some(new_selected) = selected {
+                        let ns = new_selected.clone();
+                        if ns > 0 {
+                            *selected = Some(ns - 1);
+                        }
+                    }
+                    Ok(())
+                }
+                Action::MoveDown => {
+                    if let Some(new_selected) = selected {
+                        let ns = new_selected.clone();
+                        if ns + 1 < filtered_lines.len() {
+                            *selected = Some(ns + 1);
+                        }
+                    }
+                    Ok(())
+                }
+                Action::Other => match key.code {
+                    KeyCode::Char(c) => {
+                        if *cursor_position <= input.len() {
+                            input.insert(*cursor_position, c);
+                            *cursor_position += 1;
+                        }
+                        do_filter(filtered_lines, &all_lines, &input, selected);
+                        Ok(())
+                    }
+                    KeyCode::Backspace => {
+                        if *cursor_position > 0 {
+                            input.remove(*cursor_position - 1);
+                            *cursor_position -= 1;
+                        }
+                        do_filter(filtered_lines, &all_lines, &input, selected);
+                        Ok(())
+                    }
+                    _ => Ok(()),
+                },
+            }
+        }
+        _ => Ok(()),
+    }
+}
+
